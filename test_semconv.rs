d45225b87696850c6431d03f7f4072746127bf17
Richard <shadowrhyder@gmail.com>
2025-09-04 10:02:52 +0200

    Delete test_semconv.rs

--- test_semconv.rs
+++
@@ -1,6 +0,0 @@
-use opentelemetry_semantic_conventions as semconv;
-
-fn main() {
-    println!("DB_SYSTEM_NAME: {}", semconv::attribute::DB_SYSTEM_NAME.as_str());
-    println!("DB_OPERATION_NAME: {}", semconv::attribute::DB_OPERATION_NAME.as_str());
-}
